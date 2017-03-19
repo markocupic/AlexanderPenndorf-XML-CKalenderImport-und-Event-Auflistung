761d38c1d241aa6d9f25271f97b7dce120309859
Marko Cupic <m.cupic@gmx.ch>
2017-03-19 11:29:19 +0100

    Updated explain.php

--- TL_ROOT/system/modules/ckalender_xml_event_import/languages/en/explain.php
+++ TL_ROOT/system/modules/ckalender_xml_event_import/languages/en/explain.php
@@ -3,4 +3,4 @@
 
 
 // Explanation
-$GLOBALS['TL_LANG']['XPL']['ckal_url'] = '<h2>Zeitraum dynamisch setzen</h2><p>Mit den Variablen <strong>###+*days###</strong> oder <strong>###-*days###</strong> können die URL-Parameter VonDatumD und BisDatumD dynamisch gesetzt werden. Wobei * immer f&uuml;r die Anzahl Tage steht.</p><p>Beispielsweise:<br>https://www.ckalender.de/modul_termine_xml.php?OrgID=7253&OrgPW=ac0de270b2d6494233793b0beac455d0&VonDatumD=###-2days###&BisDatumD=###+30days###&Zyklisch=nein&Gruppen=*</p>';
+$GLOBALS['TL_LANG']['XPL']['ckal_url'] = '<h2>Zeitraum dynamisch setzen</h2><p>Mit den Variablen <strong>###+*days###</strong> oder <strong>###-*days###</strong> können die URL-Parameter VonDatumD und BisDatumD dynamisch gesetzt werden. Wobei * immer f&uuml;r die Anzahl Tage steht.</p><p>Beispielsweise:<br>https://www.ckalender.de/modul_termine_xml.php?OrgID=2213&OrgPW=a3423423423423379dfgdgac455d0&VonDatumD=###-2days###&BisDatumD=###+30days###&Zyklisch=nein&Gruppen=*</p>';
